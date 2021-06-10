e00f7525d269cf7ba61c8a1f8dc16da1e2557c23
YUJI <mail@yuji.ne.jp>
2021-06-10 22:14:13 +0900

    Don't use Image API

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import React from 'react'
 
 const About: NextPage = () => {
@@ -12,7 +11,7 @@ const About: NextPage = () => {
         <p>CSVX is an extension of CSV format. This extension allows you to use CSV as a spreadsheet.</p>
         <p>Current CSVX library is implemented by Rust 🦀.</p>
         <p><a href='https://github.com/yujixr/csvx' target="_blank" rel="noreferrer" className='pure-button pure-button-primary'>Visit GitHub repo</a></p>
-        <Image className='pure-img' src='/images/example.png' width='1464' height='1228' />
+        <img className='pure-img' src='/images/example.png' width='1464' height='1228' />
         <h2>About this spreadsheet application</h2>
         <p>This application is built with Next.js, WebAssembly. Internally, this uses CSVX.</p>
         <p><a href='https://github.com/yujixr/csvx-app' target="_blank" rel="noreferrer" className='pure-button pure-button-primary'>Visit GitHub repo</a></p>
